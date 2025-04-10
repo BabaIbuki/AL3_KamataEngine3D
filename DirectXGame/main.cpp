aa8cb29f8da004a932e6ca8d9c50eb97058886d7
LC1A_22_Baba_Ibuki <k024g0056@g.neec.ac.jp>
2025-04-10 19:59:47 +0900

    変更

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,48 @@
 #include <Windows.h>
 
+// KamataEngineのインクルード
+#include "KamataEngine.h"
+// Sceneのインクルード
+#include "GameScene.h"
+using namespace KamataEngine;
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
+	// KamataEngineの初期化
+	KamataEngine::Initialize(L"LD2A_05_ババ_イブキ_AL3");
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	// ゲームシーンのインスタンス作成と初期化
+	GameScene* gameScene = new GameScene();
+	gameScene->Initialize();
+
+	while (true) {
+		// KamataEngineの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// ゲームシーンの更新
+		gameScene->Update();
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// ここに描画処理を記述する
+		gameScene->Draw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	// ゲームシーンの開放
+	delete gameScene;
+	gameScene = nullptr;
+
+	// KamataEngineの終了
+	KamataEngine::Finalize();
+
 	return 0;
 }
